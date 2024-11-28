584c3b44152019b8767b0c8cd2ba78a8848a19dd
Tom Dowley <tom.dowley@cosine.sh>
2024-11-28 11:40:09 +0000

    refactor: simplify useEffect import in App component
    Co-authored-by: Genie <genie@cosine.sh>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  React.useEffect(() => {
+  useEffect(() => {
     document.title = "hi";
   }, []);
 
