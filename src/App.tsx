d467116f8447f08dafcbb89bb9b114875f14d82b
Tom Dowley <tom.dowley@cosine.sh>
2024-11-29 10:20:05 +0000

    fix: remove duplicate import of React
    Co-authored-by: Genie <genie@cosine.sh>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function App() {
   React.useEffect(() => {
     document.title = "yo";
   }, []);
