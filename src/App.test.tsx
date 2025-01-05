e65265a2308686860bd19db366e3eb8b68308705
Tom Dowley <tom.dowley@cosine.sh>
2025-01-05 18:33:21 +0000

    fix: update import for act from react instead of react-dom/test-utils
    Co-authored-by: Genie <genie@cosine.sh>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
+import { act } from "react";
 import App from "./App";
 
 test("sets document title to Hi", () => {
