7e04da6e6ed2a66cf8017b30cb936afe58e80ef3
Tom Dowley <tom.dowley@cosine.sh>
2025-02-14 13:51:02 +0000

    fix: update test to use ReactDOMTestUtils for act function
    Co-authored-by: Genie <genie@cosine.sh>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from './App';
-// import { ReactDOMTestUtils } from 'react-dom/test-utils';
+import ReactDOMTestUtils from 'react-dom/test-utils';
 
-import { act } from "react";
+
 
 describe('simple', () => {
   it('renders learn react link', () => {
-    act(() => {
+    ReactDOMTestUtils.act(() => {
       render(<App />);
     })
   
