26b7eef9a07da90dd74ecf24202ee1434859b8de
Tom Dowley <tom.dowley@cosine.sh>
2025-02-17 13:30:51 +0000

    test: add additional assertion in App component test
    Co-authored-by: Genie <genie@cosine.sh>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,12 @@
 import { act, render, renderHook, screen } from "@testing-library/react";
 import App from "./App";
-import { BrowserRouter, Link } from "react-router-dom";
 
 describe("renders", () => {
 	test("renders learn react link", async () => {
 	render(<App />);
 
 		expect(screen.getByText("Learn React")).toBeTruthy();
+
+		expect(true).toBe(true);
 	});
 });
