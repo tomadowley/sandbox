9934ef8de2a87d45586e89048e993a2f195f9263
Tom Dowley <tom.dowley@cosine.sh>
2025-02-17 11:58:30 +0000

    test: update App component tests to include translation mock and check navbar presence
    Co-authored-by: Genie <genie@cosine.sh>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,24 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { act } from "react";
+import { mocked } from "ts-jest/utils";
+import { useTranslation } from "react-i18next";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-  expect(false).toBe(true);
+jest.mock("react-i18next");
+
+const mockedUseTranslation = mocked(useTranslation);
+describe("App", () => {
+  mockedUseTranslation.mockImplementation(() => ({
+    t: (str) => str,
+    i18n: {
+      changeLanguage: jest.fn(),
+      language: "pl",
+    },
+  }));
+  test("renders learn react link", () => {
+    act(() => {
+      render(<App />);
+      expect(screen.getByTestId("navbar")).toBeInTheDocument();
+    });
+  });
 });
