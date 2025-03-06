d79b8b2c19c4b645c60725013d15bd5f9263c4dd
Tom Dowley <tom.dowley@cosine.sh>
2025-03-06 11:24:50 +0000

    feat: add a collection of jokes to the application
    Co-authored-by: Genie <genie@cosine.sh>

---
+++ src/jokes.ts
@@ -0,0 +1,41 @@
+// Collection of jokes for the application
+export interface Joke {
+  id: number;
+  setup: string;
+  punchline: string;
+}
+
+export const jokes: Joke[] = [
+  {
+    id: 1,
+    setup: "Why don't scientists trust atoms?",
+    punchline: "Because they make up everything!"
+  },
+  {
+    id: 2,
+    setup: "How does a computer get drunk?",
+    punchline: "It takes screenshots!"
+  },
+  {
+    id: 3,
+    setup: "Why did the developer go broke?",
+    punchline: "Because he used up all his cache!"
+  },
+  {
+    id: 4,
+    setup: "Why do programmers always mix up Halloween and Christmas?",
+    punchline: "Because Oct 31 == Dec 25!"
+  },
+  {
+    id: 5,
+    setup: "What's a programmer's favorite place to hang out?",
+    punchline: "Foo Bar!"
+  },
+  {
+    id: 6,
+    setup: "Why did the JavaScript developer wear glasses?",
+    punchline: "Because they couldn't C#!"
+  }
+];
+
+export default jokes;
