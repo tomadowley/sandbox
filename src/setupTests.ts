737c475284e0c8ae303bd3a049ad75abd4b7269e
Tom Dowley <tdowley@live.co.uk>
2025-04-03 16:29:53 +0100

    chore: update setupTests to import act from React and remove deprecated react-dom/test-utils
    Co-authored-by: Genie <genie@cosine.sh>

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import React from 'react';
+
+// Use act from React instead of react-dom/test-utils (which is deprecated)
+export { act } from 'react';
