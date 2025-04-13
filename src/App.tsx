9dc584a32507abe4beb41805f0ae180fae7dc1eb
Tom Dowley <tdowley@live.co.uk>
2025-04-13 15:19:49 +0100

    chore: add comments and documentation to App component
    Co-authored-by: Genie <genie@cosine.sh>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
+// Import React library - the core of React functionality
 import React from 'react';
+// Import the logo SVG file to be used in the component
 import logo from './logo.svg';
+// Import CSS styles specific to this component
 import './App.css';
 
+/**
+ * App Component
+ * 
+ * This is the main component rendered in the application.
+ * It displays the React logo, a text prompt to edit the file,
+ * and a link to the React documentation.
+ */
 function App() {
   return (
+    // Main container div with App class
     <div className="App">
+      {/* Header section containing logo and content */}
       <header className="App-header">
+        {/* React logo with spinning animation */}
         <img src={logo} className="App-logo" alt="logo" />
+        {/* Prompt message for developers */}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        {/* Link to React documentation */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,4 +38,5 @@ function App() {
   );
 }
 
+// Export the App component as the default export so it can be imported in other files
 export default App;
