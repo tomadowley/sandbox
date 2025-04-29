5d2c95835417fafc8fafaee615a5e194927c3000
Tom Dowley <tdowley@live.co.uk>
2025-04-29 17:02:50 +0000

    test: update test case to check for infinity mirror simulator caption
    Co-authored-by: Genie <genie@cosine.sh>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders infinity mirror simulator caption", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-  expect(false).toBe(true);
+  const captionElement = screen.getByText(/infinity mirror simulator/i);
+  expect(captionElement).toBeInTheDocument();
 });
